33c2c6b8e5fc7bd3d9e37b3e359ad00754c48ba0
vjblind <vjblind@users.noreply.github.com>
2017-12-26 17:58:45 +0100

    cuda inside unity level 0

---
+++ Test1.cs
@@ -0,0 +1,30 @@
+﻿//copy HelloWorldCUDA.dll into Assets/Plugins directory
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Test1 : MonoBehaviour {
+
+    // The imported function
+    [DllImport("test", EntryPoint = "TestSort")]
+    public static extern void TestSort(int [] a, int length);
+ 
+    public int[] a;
+ 
+    void Start() {
+        TestSort(a, a.Length);
+    }
+}
+   
+
+
+
+     
+ 
+
+
+
+ 
+ 
